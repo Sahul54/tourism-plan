741d085c578c7dcb6c9c29df892dd35cca2c4013
Sahul54 <ksahul9576@gmail.com>
2024-08-12 01:14:33 +0530

    TourismApp

---
+++ src/components/Tours.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Card from './Card'
+
+const Tours = ({tours, removeTour}) => {
+  return (
+    <div className='container'>
+      <div className='title'>
+        <h2>Plane with Love</h2>
+      </div>
+      <div className='cards'>
+        {
+          tours.map( (tour) => {
+              return <Card {...tour} removeTour={removeTour}></Card>
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Tours
